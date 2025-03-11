8f73f33ef8f706d52442d18c283e01ada818923e
Kris Home <kris@luda.no>
2025-03-11 11:28:28 +0100

    added a couple of modules to build (niagara, umg)

--- Source/Imp/Imp.Build.cs
+++ Source/Imp/Imp.Build.cs
@@ -7,22 +7,19 @@ public class Imp : ModuleRules {
 	public Imp(ReadOnlyTargetRules Target) : base(Target) {
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-		//Add to the above based on Starmasons? {, "OnlineSubsystem", "OnlineSubsystemEOS", "Niagara", "EditorLib"}
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "OnlineSubsystem", "OnlineSubsystemEOS", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 
 		PublicIncludePaths.AddRange(new string[] {
 			ModuleDirectory,
 			Path.Combine(ModuleDirectory, "Player"),
+			Path.Combine(ModuleDirectory, "Game"),
 		});
 	}
 }
