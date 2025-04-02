388d3474482cf194e78a77e852a209e2e92bbe28
Kris Home <kris@luda.no>
2025-04-02 13:28:40 +0200

    minor

--- Source/Imp/UI/WidgetControllers/InventoryWidgetController.h
+++ Source/Imp/UI/WidgetControllers/InventoryWidgetController.h
@@ -13,9 +13,7 @@ struct FPackagedInventory;
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FInventoryItemSignature, const FMasterItemDefinition&, Item);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FInventoryBroadcastComplete);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FScrollBoxResetSignature);
-/**
- * 
- */
+
 UCLASS(Blueprintable, BlueprintType)
 class IMP_API UInventoryWidgetController : public UImpWidgetController {
 	GENERATED_BODY()
@@ -34,11 +32,11 @@ public:
 	void SetOwningActor(AActor* InOwner);
 
 	void BindCallbacksToDependencies();
-	void BroadcastInitialValues();	
+	void BroadcastInitialValues();
 
 	void UpdateInventory(const FPackagedInventory& InventoryContents);
 	void BroadcastInventoryContents();
-	
+
 	UPROPERTY()
 	TObjectPtr<AActor> OwningActor;
 
